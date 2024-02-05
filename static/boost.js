7cf86b9038d1026f6657e69c226992559af98595
Joseph Halla <joseph.halla@park.edu>
2024-02-05 16:38:51 -0700

    boost tweaks

--- static/boost.js
+++ static/boost.js
@@ -1,10 +1,14 @@
 import { updateSliderValues } from "./slider.js";
 import {calculateECR} from "./fuel.js"
 
-export const atmosphere = 14.7;
+export const atmosphere_sea_level = 14.7;
 
 function boostRatio(atmosphere, psi) {
  return ((atmosphere + psi) / atmosphere);
+}
+
+function altitude_pressure(alt){
+  return atmosphere_sea_level/(alt/1000);
 }
 
 export function boostCalc() {
@@ -12,17 +16,15 @@ export function boostCalc() {
   const boostValue = Number(document.getElementById("boostPsi").value);
   var maxECR = Number(document.getElementById("maxECR").textContent);
   var ehp = Number(document.getElementById("nahp").value);
+  // var altitude = Number(document.getElementById("altitude").value);
 
-  var br = boostRatio(atmosphere,boostValue);
-  console.log(ehp);
-  // console.log(Math.floor(ehp * br));
+  var br = boostRatio(atmosphere_sea_level,boostValue);
 
   document.getElementById("boostHP").textContent = Math.floor(ehp * br);
   var ecr = (cr*br).toFixed(1);
   
   const ecrLabel = document.getElementById('ecr');
   ecrLabel.textContent = ecr;
-
 
   if (ecr > maxECR) {
       ecrLabel.classList.add('red-label');
@@ -32,5 +34,5 @@ export function boostCalc() {
 }
 
 // Add event listener for fuel type selection
-updateSliderValues(['boostPsi', 'compRatio', 'nahp'], boostCalc);
+updateSliderValues(['boostPsi', 'compRatio', 'nahp', 'altitude'], boostCalc);
 document.getElementById('fuelType').addEventListener('input', calculateECR);
