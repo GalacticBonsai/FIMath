0d887c628965cf01b6eb0a630b32fbd199325933
Joseph Halla <joseph.halla@park.edu>
2024-02-05 13:37:34 -0700

    Split into multiple pages

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from views import views
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.register_blueprint(views)
 
-
 if __name__ == '__main__':
-
-  app.run(debug=True, port=8080)
+    app.run(debug=True, port=5000)
--- views.py
+++ views.py
@@ -4,4 +4,12 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-  return render_template("index.html")
+    return render_template("index.html")
+
+@views.route("/engine")
+def engine():
+    return render_template("engine.html")
+
+@views.route("/boosted")
+def boosted():
+    return render_template("boosted.html")
